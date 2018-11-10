7eb2935ee97471db4ae466a1a61875d05f6c4d29
Miguel 'Mikasonwar' Pereira <mikasisto2@gmail.com>
2018-11-10 16:40:45 +0000

    Init

---
+++ autoignicao.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+
+
+    //console.log("Teste");
+    console.log($('input[name=armadura]:checked', '#armadura').val());
+
+    
+    $("#fatura").click(function(){
+        //console.log($('input[name=armadura]:checked', '#armadura').val());
+        
+        var Nome = ["armadura","motor","suspensao","transmissao","travao","turbo"];
+        var soma = 0
+        for(var i=0;i<Nome.length;i++) {
+            if($("input[name="+Nome[i]+"]:checked").siblings("input[type=number]").val()!=undefined) {
+                soma+=parseInt($("input[name="+Nome[i]+"]:checked").siblings("input[type=number]").val());
+            }
+        }
+
+        soma = Math.ceil(soma * (1 + ($("#percentagem").val()/100)));
+        $("#resposta").empty().append("Valor Total: "+soma);
+
+    });
+
+    $("#carro").change(function(){
+        var valor = $("#carro").val();
+        $("input[type=radio]").each(function(index){
+            $(this).siblings("input[type=number]").val(Math.ceil(valor * $(this).val()));
+        });
+    });
+
+    $("#turbo").change(function(){
+        var turbo = $("#turbo").val();
+        turbo= turbo / 0.104375
+        $("#carro").val(Math.ceil(turbo));
+        $("#carro").trigger("change");
+    });
+
+});
