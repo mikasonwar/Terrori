8f1390c0cbb5a2a207e43da17451d199f00475da
Miguel 'Mikasonwar' Pereira <ocultado@privacy.com>
2020-05-07 17:59:47 +0100

    Atualização para terrori

--- js/terrori.js
+++ js/terrori.js
@@ -8,8 +8,6 @@ $(document).ready(function() {
         $("#switchColor").html('Tema Escuro');
     }
 
-    //console.log("Teste");
-    console.log($('input[name=armadura]:checked', '#armadura').val());
     $("#switchColor").click(function(){
         if($("body").hasClass("white")) {            
             $("body").addClass("black").removeClass("white");
@@ -29,20 +27,24 @@ $(document).ready(function() {
         var Nome = ["armadura","motor","suspensao","transmissao","travao","turbo"];
         var soma = 0
         for(var i=0;i<Nome.length;i++) {
-            if($("input[name="+Nome[i]+"]:checked").siblings("input[type=number]").val()!=undefined) {
-                soma+=parseInt($("input[name="+Nome[i]+"]:checked").siblings("input[type=number]").val());
+            let $input = $("input[name="+Nome[i]+"]:checked").closest(".input-group").children("input[type=number]");
+            if($input.val()!=undefined) {
+                soma+=parseInt($input.val());
             }
         }
 
         soma = Math.ceil(soma * (1 + ($("#percentagem").val()/100)));
         $("#resposta").empty().append("Valor Total: "+soma);
+        $("#alert").show();
 
     });
 
     $("#carro").change(function(){
         var valor = $("#carro").val();
         $("input[type=radio]").each(function(index){
-            $(this).siblings("input[type=number]").val(Math.ceil(valor * $(this).val()));
+            let $input = $(this).closest(".input-group").children("input[type=number]");
+            // $(this).siblings("input[type=number]").val(Math.ceil(valor * $(this).val()));
+            $input.val(Math.ceil(valor * $(this).val()));
         });
     });
 
